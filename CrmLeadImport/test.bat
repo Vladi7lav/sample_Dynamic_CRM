18eb49328d5cfe0dfa14e75a9f4c1e68df76054e
Vladi7lav <33389913+Vladi7lav@users.noreply.github.com>
2019-05-13 15:28:18 +0300

    Delete test.bat

--- CrmLeadImport/test.bat
+++
@@ -1,3 +0,0 @@
-cd /D "%~dp0"  
-CrmSvcUtil.exe /url:http://10.60.200.48:5555/CRM2016/XRMServices/2011/Organization.svc?wsdl /out:Context.cs /serviceContextName:Context /n:CRM /u:Administrator /p:Pass@word99
-pause
